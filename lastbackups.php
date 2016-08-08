442849bf8c5249bee0301494e0e37777a62b6d83
Steffen Pegenau <steffen.pegenau@hrz.tu-darmstadt.de>
2016-08-08 12:27:40 +0200

    Tried to mark unwanted backups. Related to #3

--- lastbackups.php
+++ lastbackups.php
@@ -24,12 +24,47 @@ function formatBytes($size, $precision = 2)
     return round(pow(1024, $base - floor($base)), $precision) .' '. $suffixes[floor($base)];
 }
 
+
+
 require_once '../../config.php';
 
 global $DB, $CFG;
 
 require_login();
 require_capability('local/categorybackup:view', context_system::instance());
+
+
+$output = "<html><head>";
+
+// Bootstrap
+$output .= '
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>';
+
+$output .= "</head><body><div class='container'>";
+
+$category_ids = explode(',', $CFG->local_categorybackup_categories);
+
+$output .= "<h4>Backups sollten fuer diese Kategorien erstellt werden:</h4>";
+$output .= "<ul>";
+require_once($CFG->dirroot . '/lib/coursecatlib.php');
+
+$catnames = [];
+foreach ($category_ids as $id) {
+	$cat = coursecat::get($id);
+	$catnames[] = $cat->name;
+	$output .= "<li>" . $cat->name . "</li>";
+}
+
+$output .= "</ul>";
+
 
 require_once $CFG->libdir . '/outputcomponents.php';
 
@@ -68,48 +103,33 @@ $courseId = "Kurs ID";
 $fullname = "Kurs";
 $created = "Backup erstellt";
 $size = "Backup Groesse";
+$unwanted = "Ungewollt";
 
 $table = new html_table();
 $table->attributes = array("class" => "table table-striped table-bordered table-hover table-condensed table-responsive");
-$table->head = array($courseId, $term, $department, $fullname, $created, $size);
+$table->head = array($courseId, $term, $department, $fullname, $created, $size, $unwanted);
 
+function isWantedBackup($semester, $fb) {
+	foreach ($catnames as $name) {
+		if($semester === $name || $fb === $name) {
+			return true;
+		}
+	}
+	return $false;
+}
 
 $date_format = "d.m.Y H:i:s";
 foreach ($results as $fileid => $f) {
     $size = formatBytes($f->filesize);
     $created = date($date_format, $f->timecreated);
     $modified = date($date_format, $f->timemodified);
+    
+    $unwanted = !isWantedBackup($f->semester, $f->fb) ? "!!!JA!!!" : "";
     $table->data[] = array($f->course, $f->semester, $f->fb,  $f->fullname, $created, $size);
 }
 
 
-$output = "<html><head>";
 
-// Bootstrap
-$output .= '
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>    
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
-
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>';
-
-$output .= "</head><body><div class='container'>";
-
-$category_ids = explode(',', $CFG->local_categorybackup_categories);
-
-$output .= "<h4>Backups sollten fuer diese Kategorien erstellt werden:</h4>";
-$output .= "<ul>";
-require_once($CFG->dirroot . '/lib/coursecatlib.php');
-foreach ($category_ids as $id) {
-    $cat = coursecat::get($id);
-    $output .= "<li>" . $cat->name . "</li>";
-}
-
-$output .= "</ul>";
 
 // Now the table
 $output .= "<h4>Bereits erstellte Backups (aus Tabelle <i>files</i> ermittelt):</h4>";
